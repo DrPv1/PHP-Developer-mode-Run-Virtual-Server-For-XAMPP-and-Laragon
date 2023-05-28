b707159ed268a8173e424333412456d32101d260
DoctorP <134605186+DrPv1@users.noreply.github.com>
2023-05-28 19:16:36 +0700

    V.1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Virtual-Server-PHP-Developer-mode-For-XAMPP-and-Laragon
- cmd Project
+# Virtual Server PHP Developer mode
+Made for beginners with Batch language
+how to use
+1. Open XAMPP's DevelopmentServerStart.bat file or Laragon
+2. input Name your project folder. to start the server
+3. Wait until the CMD says that the server is ready.
+4. Open your browser http://localhost:8080
+5.Done
---
+++ XAMPP/DevelopmentServerStart.bat
@@ -0,0 +1,82 @@
+cd C:\xampp\htdocs\
+@echo off
+color a
+cls
+:input
+echo.
+echo            ================================================================
+echo            ================================================================
+echo            ==  DDDDD    OOOOO  CCCCC  TTTTTTT  OOOOO   RRRRR    PPPPP    ==
+echo            ==  D    D   O   O  C         T     O   O   R   R    P    P   ==
+echo            ==  D     D  O   O  C         T     O   O   R   R    P    P   ==
+echo            ==  D     D  O   O  C         T     O   O   RRRR     PPPPP    ==
+echo            ==  D     D  O   O  C         T     O   O   R R      P        ==
+echo            ==  D    D   O   O  C         T     O   O   R  RR    P        ==
+echo            ==  DDDDD    OOOOO  CCCCC     T     OOOOO   R   RR   P        ==
+echo            ================================================================
+echo            ================================================================
+echo            ========= System Start Server Development For [XAMPP] ==========
+echo.
+set /p project=Enter your Name Project to Start Server: 
+cls
+echo Please wait, the system is processing.
+timeout /t 2
+cls
+echo.
+echo            ================================================================
+echo            ================================================================
+echo            ==  DDDDD    OOOOO  CCCCC  TTTTTTT  OOOOO   RRRRR    PPPPP    ==
+echo            ==  D    D   O   O  C         T     O   O   R   R    P    P   ==
+echo            ==  D     D  O   O  C         T     O   O   R   R    P    P   ==
+echo            ==  D     D  O   O  C         T     O   O   RRRR     PPPPP    ==
+echo            ==  D     D  O   O  C         T     O   O   R R      P        ==
+echo            ==  D    D   O   O  C         T     O   O   R  RR    P        ==
+echo            ==  DDDDD    OOOOO  CCCCC     T     OOOOO   R   RR   P        ==
+echo            ================================================================
+echo            ================================================================
+echo            =============== System Start Server Development ================
+echo.
+echo.
+echo            ======================= Validate PHP Prat ======================
+timeout /t 1 > nul
+cd %project%
+if errorlevel 1 (
+    timeout /t 1 > nul
+    echo.
+    echo Invalid project name or invalid XAMPP path, please try again.
+    echo.
+    echo.
+    timeout /t 1 > nul
+    pause
+    cls
+    goto input
+)
+timeout /t 2 > nul
+echo            ======================== Your PHP Version ======================
+php -v
+timeout /t 2 > nul
+echo            ===================== Start Server Port 8080 ===================
+timeout /t 1 > nul
+timeout /t 5
+cls
+echo.
+echo            ================================================================
+echo            ================================================================
+echo            ==  DDDDD    OOOOO  CCCCC  TTTTTTT  OOOOO   RRRRR    PPPPP    ==
+echo            ==  D    D   O   O  C         T     O   O   R   R    P    P   ==
+echo            ==  D     D  O   O  C         T     O   O   R   R    P    P   ==
+echo            ==  D     D  O   O  C         T     O   O   RRRR     PPPPP    ==
+echo            ==  D     D  O   O  C         T     O   O   R R      P        ==
+echo            ==  D    D   O   O  C         T     O   O   R  RR    P        ==
+echo            ==  DDDDD    OOOOO  CCCCC     T     OOOOO   R   RR   P        ==
+echo            ================================================================
+echo            ================================================================
+echo.
+echo.
+echo            =============== Now Server is Ready IN Port 8080 ===============
+echo            ======================== Devlopment Mode =======================
+echo            ========================== Doctor[P]  ==========================
+echo.
+echo.
+php -v
+php -S localhost:8080
